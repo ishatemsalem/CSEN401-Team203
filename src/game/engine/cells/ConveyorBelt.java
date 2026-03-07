596fc513ad28fb43d2183a7c0648921e572c88ef
Rokaya Mahmoud Ahmed El-Gharib <rokayamahmoudahmedel-gharib@Rokayas-MacBook-Pro.local>
2026-03-07 02:42:49 +0200

    Added Cell class and its subclasses

--- src/game/engine/cells/ConveyorBelt.java
+++ src/game/engine/cells/ConveyorBelt.java
@@ -1,5 +1,12 @@
 package game.engine.cells;
 
-public class ConveyorBelt {
+public class ConveyorBelt extends TransportCell {
+	public ConveyorBelt(String name, int effect) {
+		super(name,effect);
+	}
+
+	
+		
+	
 
 }
